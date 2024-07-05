5f032e361472840f7d53838e845fe08506f4cf74
lotsmon <anatolii.sokolov.04@mail.ru>
2024-07-06 03:15:33 +1000

    initial0

---
+++ lib/config.dart
@@ -0,0 +1,6 @@
+class Config {
+  static final Uri launcherJarUrl =
+      Uri.parse('http://launcher.xodze.com:9274/ModtLauncher.jar');
+
+  static final String projectDirectoryName = 'modtlauncher-app';
+}
